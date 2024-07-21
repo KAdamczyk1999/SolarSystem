8160c6c8b8fdb2e9d43da2e9e6ca6f8b70109a87
KAdamczyk <krzysztofadamczyk1999@gmail.com>
2024-07-21 16:12:15 +0200

    Code refactored

--- include/View/shape_stash.h
+++ include/View/shape_stash.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "Geometry/shapes.h"
 
-#define SHAPE_COUNT 2
-Triangle myShapes[] = {{{-.25f, 0.5f}, {0.0f, .75f}, {.25f, 0.5f}}, {{-.25f, -0.25f}, {0.0f, -.0f}, {.25f, -0.25f}}};
+#define SHAPE_COUNT 3
+Point triangle1Vertices[] = {{0.0f, -0.125f}, {0.1f, 0.0f}, {0.0f, 0.125f}};
+Point triangle2Vertices[] = {{-.125f, 0.25f}, {0.0f, .375f}, {.125f, 0.25f}};
+Point triangle3Vertices[] = {{-.25f, 0.5f}, {0.0f, .75f}, {.25f, 0.5f}};
+Shape myShapes[] = {
+    {triangle1Vertices, 3, GL_TRIANGLES},
+    {triangle2Vertices, 3, GL_TRIANGLES},
+    {triangle3Vertices, 3, GL_TRIANGLES},
+};
--- src/Geometry/shapes.c
+++ src/Geometry/shapes.c
@@ -9,23 +9,36 @@ const int maxDim = 3;
 
 float _degsToRads(float angleDegs) { return (angleDegs * PI) / 180.0f; }
 
-void mapShapeToGLVertices(Point* shape, GLfloat* vertices, int verticesCount, int dim) {
+void mapShapeToGLVertices(Shape shape, GLfloat* vertices, int dim) {
     assert(dim > 0 && dim <= maxDim);
-    for (int i = 0; i < verticesCount; i++) {
-        memcpy(&(vertices[i * maxDim]), &(shape[i]), dim * sizeof(GLfloat));
+    for (int i = 0; i < shape.verticesCount; i++) {
+        memcpy(&(vertices[i * maxDim]), &(shape.vertices[i]), dim * sizeof(GLfloat));
         memset(&(vertices[i * maxDim + dim]), 0.0f, (maxDim - dim) * sizeof(GLfloat));
     }
 }
 
-void rotateShape(Point* shape, int verticesCount, float angleDegs, Point pointOfRotation) {
+void rotateShape(Shape shape, float angleDegs, Point pointOfRotation) {
     float angleRads = _degsToRads(angleDegs);
     float cosA = cos(angleRads);
     float sinA = sin(angleRads);
-    for (int i = 0; i < verticesCount; i++) {
-        Point* point = &(shape[i]);
+    for (int i = 0; i < shape.verticesCount; i++) {
+        Point* point = &(shape.vertices[i]);
         float xDiff = point->x - pointOfRotation.x;
         float yDiff = point->y - pointOfRotation.y;
         point->x = pointOfRotation.x + (xDiff)*cosA - (yDiff)*sinA;
         point->y = pointOfRotation.y + (xDiff)*sinA + (yDiff)*cosA;
     }
+}
+
+Point getShapeCenter(Shape shape) {
+    Point point = {0.0f, 0.0f, 0.0f};
+    for (int i = 0; i < shape.verticesCount; i++) {
+        point.x += shape.vertices[i].x;
+        point.y += shape.vertices[i].y;
+        point.z += shape.vertices[i].z;
+    }
+    point.x /= shape.verticesCount;
+    point.y /= shape.verticesCount;
+    point.z /= shape.verticesCount;
+    return point;
 }
